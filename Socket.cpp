737aa43a8ffef4253bc74c9c55d28677b9f8ec02
강명구 <mg5566@hanmail.net>
2021-06-04 15:45:20 +0900

    create Socket class

--- Socket.cpp
+++ Socket.cpp
@@ -25,3 +25,69 @@ bool Socket::create() {
 
   return (true);
 }
+
+bool Socket::bind(const int port) {
+  if (!is_valid())
+    return (false);
+
+  m_addr.sin_family = AF_INET;
+  m_addr.sin_addr.s_addr = INADDR_ANY;
+  m_addr.sin_port = htons(port);
+
+  int bind_return;
+
+  bind_return = ::bind(m_socket, (struct sockaddr *)&m_addr, sizeof(struct sockaddr));
+  if (bind_return == -1)
+    return (false);
+  return (true);
+}
+
+bool Socket::listen() const {
+  if (!is_valid())
+    return (false);
+
+  int listen_result;
+
+  // Socket file descriptor | max connetor
+  // 향후 nginx configuration 을 보고 수정이 필요할듯?!
+  listen_result = ::listen(m_socket, MAX_CONNECTOR);
+
+  if (listen_result == -1)
+    return (false);
+  return (true);
+}
+
+bool Socket::accept(Socket &new_socket) const {
+  int addr_len = 0;
+  std::cout << "test addr_len : " << addr_len << std::endl;
+  // client 의 socket fd 를 받아옵니다.
+  new_socket.m_socket = ::accept(m_socket, (sockaddr *)&m_addr, (socklen_t *)&addr_len);
+  std::cout << "test addr_len : " << addr_len << std::endl;
+
+  if (new_socket.m_socket == -1)
+    return (false);
+  return (true);
+}
+
+bool Socket::send(const std::string mes) const {
+  int status = ::send(m_socket, mes.data(), mes.size(), 0);
+  if (status == -1)
+    return (false);
+  return (true);
+}
+
+int Socket::recv(std::string &mes) const {
+  char buffer[MAX_BUFFER_SIZE + 1];
+  int status = ::recv(m_socket, buffer, MAX_BUFFER_SIZE, 0);
+
+  if (status == -1) {
+    std::cout << "ERROR: recv error" << std::endl;
+    return (0);
+  }
+  else if (status == 0)
+    return (0);
+  else {
+    mes = buffer;
+    return (status);
+  }
+}
--- Socket.hpp
+++ Socket.hpp
@@ -3,9 +3,12 @@
 
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
+#include <iostream>
 #include <sys/socket.h>  // socket
 #include <netinet/in.h>  // sockaddr_in
+
+#define MAX_BUFFER_SIZE 1024
+#define MAX_CONNECTOR 10
 
 class Socket {
   private:
@@ -17,10 +20,18 @@ class Socket {
 
     // Server
     bool create();
+    bool bind(const int port);
+    bool listen() const;
+
+    // Client initialzation
+    bool accept(Socket &new_socket) const;
+
+    // Data Transmission
+    bool send(const std::string mes) const;
+    int recv(std::string &mes) const;
 
     // util
-    bool is_valid() const { return (!m_socket != -1); }
-
+    bool is_valid() const { return (m_socket != -1); }
 };
 
 #endif
