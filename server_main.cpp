19adacb02e6249029b1e4c391f8e639bb0b14225
강명구 <mg5566@hanmail.net>
2021-06-04 16:41:40 +0900

    update server main : find loop problem....

--- server_main.cpp
+++ server_main.cpp
@@ -1,15 +1,33 @@
-#include "ServerSocket.hpp"
+/*
+#include <sys/socket.h>  // socket
+#include <netinet/in.h>  // sockaddr_in
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+*/
+
+#include "Socket.hpp"
+
+#define PORT 123456
 
 int main(void) {
-  ServerSocket server(12345);
+  Socket socket;
+  Socket new_socket;
 
-  ServerSocket server_socket;
-  server.accept(server_socket);
-
+  socket.create();
+  socket.bind(PORT);
+  socket.listen();
   while (1) {
+    socket.accept(new_socket);
+    new_socket.send("fuck you");
+
     std::string mes;
-    server_socket >> mes;
-    server_socket << mes;
+    int recv_len = new_socket.recv(mes);
+    while (recv_len > 0) {
+      std::cout << "From Client : " << mes << std::endl;
+      recv_len = new_socket.recv(mes);
+    }
   }
+
   return (0);
 }
