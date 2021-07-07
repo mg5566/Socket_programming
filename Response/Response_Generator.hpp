93fcbd5d796a1b15bbc56285348acdb5d486dc43
Hack3rs <mg5566@hanmail.net>
2021-07-07 18:22:39 +0900

    update response message generator

--- Response/Response_Generator.hpp
+++ Response/Response_Generator.hpp
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <vector>
+#include <fstream>
 
 #define SPACE " "
 #define CRLF "\r\n"
@@ -17,9 +19,11 @@ class Response_Generator {
     ~Response_Generator();
     
     void set_start_line(std::string &res_msg);
-    void set_header(std::string &res_msg);
+    void set_headers(std::string &res_msg, std::map<std::string, std::vector<std::string> > header_map);
+    void set_entity_body(std::string &res_msg, std::string file_name);
 
     // std::string get_res_msg(void);
+    std::string gen_header(std::string key, std::vector<std::string> values);
 };
 
 #endif
--- Response/test_main.cpp
+++ Response/test_main.cpp
@@ -2,16 +2,37 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 int main(void) {
+  std::string file_name = "text.txt";
+  std::map<std::string, std::vector<std::string> > header_map;
+  /* set header map */
+  std::vector<std::string> vec;
+  vec.push_back("127.0.0.1");
+  // header_map["Host"] = {"127.0.0.1"};
+  header_map["Host"] = vec; 
+  vec.clear();
+  vec.push_back("100");
+  header_map["Content-Length"] = vec; 
+  vec.clear();
+  vec.push_back("de-DE");
+  vec.push_back("en-CA");
+  header_map["Content-Lengauge"] = vec;
+  // header_map["Content-Lengauge"] = {"de-DE", "en-CA"};
+
+
   std::string res_msg;
   Response_Generator rg;
 
     // rg.set_res_msg(res_msg);
   rg.set_start_line(res_msg);
-  rg.set_header(res_msg);
+  rg.set_headers(res_msg, header_map);
+  rg.set_entity_body(res_msg, file_name);
 //   res_msg = rg.get_res_msg();
   std::cout << res_msg << std::endl;
 
+    
   return (0);
 }
